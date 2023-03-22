3c6298c26914df66d91222cc54a5ca0add633aa1
Lê Khắc Thanh Tùng <87592549+ThanhTung2410@users.noreply.github.com>
2023-03-22 09:40:10 +0700

    add folder Lab05 version1

---
+++ Lab/version1/Lab05/Lab05.py
@@ -0,0 +1,192 @@
+# Exercise 1
+
+# (a) For all fishes, they need water to survive.
+
+def ex1_a():
+    print("D is 'fishes'")
+    print("P is 'need water to survive'")
+    print("Formal form: For all x in D, P(x)")
+
+
+# ex1_a()
+
+# (b) Exist a person, who is left handed
+
+
+def ex1_b():
+    print("D is 'people'")
+    print("P is 'is left handed'")
+    print("Formal form: ∃ x in D such that P(x)")
+
+# ex1_b()
+
+# (c) Exist an employee in the company, who is late to work everyday.
+
+
+def ex1_c():
+    print("D is 'employees in the company'")
+    print("P is 'is late to work everyday'")
+    print("Formal form: ∃ x in D such that P(x)")
+
+# ex1_c()
+# (d) For all fishes in this pond, they are Koi fish.
+
+
+def ex1_d():
+    print("D is 'fishes in this pond'")
+    print("P is 'is Koi fish'")
+    print("Formal form: For all x in D, P(x)")
+
+
+# ex1_d()
+# (e) There is at least one creature in the ocean, it can live on land
+
+
+def ex1_e():
+    print("D is 'creature in the ocean'")
+    print("P is 'can live on land'")
+    print("Formal form: ∃ x in D such that P(x)")
+
+# ex1_e()
+# (f) Every students in class A did not pass the test
+
+
+def ex1_f():
+    print("D is 'students in class A'")
+    print("P is 'did not pass the test'")
+    print("Formal form: For all x in D, P(x)")
+
+# ex1_f()
+
+# Exercise 2
+
+# (a) For all people, if they are blond then they are westerners.
+
+
+def ex2_a():
+    print("D is 'people'")
+    print("P is 'is blond'")
+    print("Q is 'is westerners'")
+    print("Formal form: For all x in D, P(x) then Q(x)")
+
+# ex2_a()
+
+# (b) For all students, if they study correctly then they have high score.
+
+
+def ex2_b():
+    print("D is 'students'")
+    print("P is 'studies correctly'")
+    print("Q is 'has high score'")
+    print("Formal form: For all x in D, P(x) then Q(x)")
+
+# ex2_b()
+
+
+# (c) For every mammal, if they live in the sea, they are either dolphins or whales.
+def ex2_c():
+    print("D is 'mammal'")
+    print("P is 'lives in the sea'")
+    print("Q is 'is either dolphins or whales'")
+    print("Formal form: For all x in D, P(x) then Q(x)")
+
+# ex2_c()
+
+
+# (d) For every bird, if they don’t have wings and can swim then they are penguins.
+def ex2_d():
+    print("D is 'bird'")
+    print("P is 'does not have wings and can swim'")
+    print("Q is 'is penguin'")
+    print("Formal form: For all x in D, P(x) then Q(x)")
+
+
+# ex2_d()
+
+# Exercise 3
+
+# (a) If we turn off the water in the shower, then the water will stop pouring.
+def ex3_a():
+    print("p", "\t\t", "we turn off the water in the shower")
+    print("q", "\t\t", "the water stops pouring")
+    print("Statement", "\t",
+          "If we turn off the water in the shower, then the water will stop pouring.")
+    print("Negation", "\t",
+          "We have turned off the water in the shower and the water does not stop pouring.")
+    print("Converse", "\t",
+          "If the water stops pouring, then we have turned off the water in the shower.")
+    print("Inverse", "\t",
+          "If we do not turn off the water in the shower, then the water will not stop pouring.")
+    print("Contrapositive", "\t",
+          "If the water does not stop pouring, then we have not turned off the water in the shower.")
+
+# ex3_a()
+
+# (b) If a triangle has three congruent sides, it is an equilateral triangle.
+
+
+def ex3_b():
+    print("p", "\t\t", "a triangle has three congruent sides")
+    print("q", "\t\t", "it is an equilateral triangle")
+    print("Statement", "\t",
+          "If a triangle has three congruent sides, it is an equilateral triangle.")
+    print("Negation", "\t",
+          "A triangle has three congruent sides and it is not an equilateral triangle.")
+    print("Converse", "\t",
+          "If a triangle is an equilateral triangle, it will have three congruent sides.")
+    print("Inverse", "\t",
+          "If a triangle does not have three congruent sides, it is not an equilateral triangle.")
+    print("Contrapositive", "\t",
+          "If a triangle is not an equilateral triangle, it will not have three congruent sides.")
+
+
+# ex3_b()
+
+# (c) If you take yoga, then you are relaxed.
+
+
+def ex3_c():
+    print("p", "\t\t", "you take yoga")
+    print("q", "\t\t", "you are relaxed")
+    print("Statement", "\t", "If you take yoga, then you are relaxed.")
+    print("Negation", "\t",
+          "You have taken yoga and you are not relaxed.")
+    print("Converse", "\t", "If you are relaxed, then you are taking yoga.")
+    print("Inverse", "\t", "If you do not take yoga, then you are not relaxed.")
+    print("Contrapositive", "\t",
+          "If you are not relaxed, then you are not taking yoga.")
+
+# ex3_c()
+
+# (d) All kids like ice cream.
+
+
+def ex3_d():
+    print("p", "\t\t", "you are kid")
+    print("q", "\t\t", "you like ice cream")
+    print("Statement", "\t", "If you are kid, you will like ice cream.")
+    print("Negation", "\t",
+          "You are kid and you do not like ice cream.")
+    print("Converse", "\t", "If you like ice cream, then you are kid.")
+    print("Inverse", "\t", "If you are not kid, you will not like ice cream.")
+    print("Contrapositive", "\t",
+          "If you do not like ice cream, then you are not kid.")
+
+# ex3_d()
+
+# (e) If you do your homework, then you can watch TV.
+
+
+def ex3_e():
+    print("p", "\t\t", "you do your homework")
+    print("q", "\t\t", "you can watch TV")
+    print("Statement", "\t", "If you do your homework, then you can watch TV.")
+    print("Negation", "\t",
+          "You have done your homework and you can not watch TV.")
+    print("Converse", "\t", "If you can watch TV, then you have done your homework.")
+    print("Inverse", "\t", "If you do not do your homework, then you can not watch TV.")
+    print("Contrapositive", "\t",
+          "If you can not watch TV, then you have not done your homework.")
+
+
+# ex3_e()
